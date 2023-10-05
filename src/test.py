9c568b6d19c23727b1d320d6b83ddfd92a86b5ed
Faraz Razi <farazrazi432@gmail.com>
2023-10-05 10:50:32 +0500

    fixed tests

--- src/test.py
+++ src/test.py
@@ -1,8 +1,8 @@
 import unittest
 
 class TestMain(unittest.TestCase):
-    def sample_test(self):
-        self.assertEqual(1, 1)
+    def test_add(self):
+        self.assertEqual(3, 3)
 
 
 if __name__ == '__main__':
