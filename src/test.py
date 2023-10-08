a2ffff139838d33bac4a6312b23e556d6fd3eb35
Faraz Razi <farazrazi432@gmail.com>
2023-10-08 16:04:10 +0500

    fixed test #2

--- src/test.py
+++ src/test.py
@@ -8,15 +8,14 @@ class TestApp(unittest.TestCase):
         self.app = app.test_client()
         self.db = connect_to_db()
         self.cursor = self.db.cursor()
+        self.taskID = None
+
+        self.cursor.execute("DELETE FROM tasks")
+        self.db.commit()
+
         
-        # Create a test database or use an existing one for testing
-        self.cursor.execute("CREATE DATABASE IF NOT EXISTS test_TODO")
-        self.cursor.execute("USE test_TODO")
-        # Perform any other necessary setup for your test database
-    
     def tearDown(self):
         # Clean up the test database and close the database connection
-        self.cursor.execute("DROP DATABASE IF EXISTS test_TODO")
         self.cursor.close()
         self.db.close()
     
@@ -32,31 +31,35 @@ class TestApp(unittest.TestCase):
         }
         response = self.app.post('/addTask', data=json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
+        
+        # Retrieve the task ID from the response and store it for later use
+        self.taskID = response.json['task']['id']
 
     def test_get_tasks_route(self):
         response = self.app.get('/getTasks')
-        print(response.json)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json['tasks']), 1)
     
     def test_update_task_route(self):
-        data = {
-            "description": "Updated Description",
-            "status": "completed",
-            "dueDate": "2023-11-15"
-        }
-        response = self.app.put('/updateTask/1', data=json.dumps(data), content_type='application/json')
-        print(response.json)
-        self.assertEqual(response.status_code, 200)
+        # Check if a task ID is available
+        if self.taskID is not None:
+            data = {
+                "description": "Updated Description",
+                "status": "completed",
+                "dueDate": "2023-11-15"
+            }
+            response = self.app.put(f'/updateTask/{self.taskID}', data=json.dumps(data), content_type='application/json')
+            self.assertEqual(response.status_code, 200)
 
     def test_search_task_route(self):
         response = self.app.get('/searchTask/Task')
         self.assertEqual(response.status_code, 200)
     
     def test_delete_task_route(self):
-        response = self.app.delete('/deleteTask/1')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json['result'], True)
+        # Check if a task ID is available
+        if self.taskID is not None:
+            response = self.app.delete(f'/deleteTask/{self.taskID}')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json['result'], True)
         
 
 if __name__ == '__main__':
